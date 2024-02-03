a4ee68fa1cda361679bbc95ca5e4dba055c09467
Creativetools90 <coderx523@gmail.com>
2024-02-03 17:52:37 +0530

    learn set and met collection in dart

---
+++ collection/map/map.dart
@@ -0,0 +1,27 @@
+dynamic main() {
+  var git_api = {
+    "id": 1,
+    "repo_name": "flutter_app",
+    "total_size": "2gb",
+    "commit": "new version",
+    "user_name": "creativetools92",
+    "user_email": "creativetools_@gmail.com",
+    "git_status": "modify",
+    "git_modify": true , 
+    "git_init": true,
+    "last_chanage": "1 hr",
+    "git_branch": "-M  'main' ",
+    "Releases": false,
+    "Packages": false,
+    "language": {"html", "css", "java_script"},
+    "data": ["index.html", "style.css", "script.js"],
+    "folders_data": false, 
+    "readme_file_have": false,
+    "readme_file_data": false,
+    "description": false,
+    "stars": 0,
+    "watching": 1,
+    "forks": 0,
+  };
+  print(git_api);
+}
